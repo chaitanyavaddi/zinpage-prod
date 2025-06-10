19e481487956cab02dc005ca414586e10cccf001
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-06-10 20:36:42 +0530

    (chore) initial setup

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return """
+      <!DOCTYPE html>
+        <html>
+
+        <head>
+            <title>Zinpage</title>
+        </head>
+
+        <body>
+            <h2>Welcome To Zinpage</h2>
+            <p>
+                Generate your digital menu in just 5 mins!
+            </p>
+        </body>
+
+        </html>
+    """
