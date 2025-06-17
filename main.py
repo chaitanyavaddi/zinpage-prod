ab6fa1f0b02d46b5ac10e3d614793438a2fb149f
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-06-17 07:25:38 +0530

    added template

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-
+from supabase import create_client
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+supabase_url = "https://zkvkujdqklmtpcvnaxgz.supabase.co"
+supabase_api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inprdmt1amRxa2xtdHBjdm5heGd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4MjYyMzgsImV4cCI6MjA2NTQwMjIzOH0.Ql9kzHB4QJDA1NaDqhXXf2xXZBkIPt_pxfp1Id07AKE"
+
+database = create_client(supabase_url, supabase_api_key)
 
 @app.get("/", response_class=HTMLResponse)
-def home():
-    return """
-      <!DOCTYPE html>
-        <html>
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-        <head>
-            <title>Zinpage</title>
-        </head>
 
-        <body>
-            <h2>Welcome To Zinpage</h2>
-            <p>
-                Generate your digital menu in just 5 mins!
-            </p>
-            <a href="mailto:hellochaitanyavaddi@gmail.com">The work is inprogess</a>
-        </body>
-
-        </html>
-    """
+@app.get("/restaurants")
+def get_restaurants():
+    response = database.table('restaurants').select("*").execute()
+    return response.data
